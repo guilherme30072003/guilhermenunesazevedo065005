b31760a75e71559bd3be6e8a5ad06a85a952d784
Guilherme Azevedo <guilherme30072003@gmail.com>
2026-02-01 01:50:32 -0400

    Adiciona texto a tela inicial

--- pets-mt/src/App.tsx
+++ pets-mt/src/App.tsx
@@ -1,15 +1,18 @@
 import SearchBar from "./components/search-bar"
+import Text from "./components/text"
 import Home from "./routes/home"
 
 
 export default function App() {
   return (
     <main className={`
-          py-28 px-4 sm:px-10
+          py-16 px-4 sm:px-10
           flex-1 sm:flex-row
           items-center sm:items-stretch
           gap-2,
           `}>
+      <Text variant="blast" className="text-blue-500 flex items-center justify-center"> 🐶 Pets MT </Text>
+      <Text variant="default" className="text-(--text-secondary) flex items-center justify-center"> clique no card para ver os detalhes do pet </Text>
       <SearchBar />
       <Home />
     </main>
